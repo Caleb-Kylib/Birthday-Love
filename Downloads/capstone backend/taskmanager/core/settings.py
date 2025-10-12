5561dd8acee9178bd5113ab14f4e7769474ec039
Your Name <lemayiancaleb18@gmail.com>
2025-10-12 23:12:50 +0300

    Initial homepage setup with Bootstrap and basic API layout

--- Downloads/capstone backend/taskmanager/core/settings.py
+++ Downloads/capstone backend/taskmanager/core/settings.py
@@ -28,6 +28,11 @@ INSTALLED_APPS = [
     'tasks',
 ]
 
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
